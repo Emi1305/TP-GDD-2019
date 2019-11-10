4441f1970a16609c0149c91990fcd408447a1fd7
Emi1305 <edelepon@gmail.com>
2019-11-10 13:12:43 -0300

    Primer commit de la solucion, form de login y menu (Sin filtrar por rol todavia)

---
+++ FrbaOfertas/Login.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrbaOfertas
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (passwordValida())
+            {
+                this.textBox_usuario.Clear();
+                this.textBox_password.Clear();
+                this.Hide();
+                MenuForm menu = new MenuForm(this);
+                menu.Show();
+            }
+
+        }
+
+        private bool passwordValida()
+        {
+            // TODO: Validar la password
+            return true;
+        }
+    }
+}
---
+++ FrbaOfertas/MenuForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrbaOfertas
+{
+    public partial class MenuForm : Form
+    {
+        private delegate void MostrarMenus();
+        private MostrarMenus mostrarMenusValidos;
+        private Form loginForm;
+
+        public MenuForm(Form f )
+        {
+            InitializeComponent();
+            loginForm = f;
+        }
+
+        private void MenuForm_Load(object sender, EventArgs e)
+        {
+            //this.mostrarMenusValidos();
+        }
+
+        private void buttonSalir_Click(object sender, EventArgs e)
+        {
+            loginForm.Show();
+            this.Close();
+
+        }
+    }
+}
