4ba030f48e291b354fc01bf653791b0a36df050b
Pedro Bruno <pedroabruno@gmail.com>
2019-11-23 22:02:18 -0300

    se sube conexion a bd

--- FrbaOfertas/AbmRol/FormRolFuncionalidad.Designer.cs
+++ FrbaOfertas/AbmRol/FormRolFuncionalidad.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace FrbaOfertas.AbmRol
 {
-    partial class FormRol
+    partial class FormRolFuncionalidad
     {
         /// <summary>
         /// Required designer variable.
@@ -31,35 +31,30 @@
             this.gridRoles = new System.Windows.Forms.DataGridView();
             this.btnAceptar = new System.Windows.Forms.Button();
             this.btnVolver = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.btnAgregar = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.txtRol = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.gridRoles)).BeginInit();
             this.SuspendLayout();
             // 
             // gridRoles
             // 
             this.gridRoles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.gridRoles.Location = new System.Drawing.Point(13, 152);
+            this.gridRoles.Location = new System.Drawing.Point(24, 287);
             this.gridRoles.Name = "gridRoles";
-            this.gridRoles.Size = new System.Drawing.Size(296, 194);
+            this.gridRoles.Size = new System.Drawing.Size(217, 220);
             this.gridRoles.TabIndex = 0;
             this.gridRoles.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.gridRoles_CellContentClick);
             // 
             // btnAceptar
             // 
-            this.btnAceptar.Location = new System.Drawing.Point(211, 370);
+            this.btnAceptar.Location = new System.Drawing.Point(143, 533);
             this.btnAceptar.Name = "btnAceptar";
             this.btnAceptar.Size = new System.Drawing.Size(98, 41);
             this.btnAceptar.TabIndex = 1;
-            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.Text = "Agregar Rol";
             this.btnAceptar.UseVisualStyleBackColor = true;
             // 
             // btnVolver
             // 
-            this.btnVolver.Location = new System.Drawing.Point(13, 370);
+            this.btnVolver.Location = new System.Drawing.Point(24, 533);
             this.btnVolver.Name = "btnVolver";
             this.btnVolver.Size = new System.Drawing.Size(95, 41);
             this.btnVolver.TabIndex = 2;
@@ -67,67 +62,19 @@
             this.btnVolver.UseVisualStyleBackColor = true;
             this.btnVolver.Click += new System.EventHandler(this.button1_Click);
             // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(115, 98);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 3;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(18, 103);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(84, 13);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Funcionalidades";
-            // 
-            // btnAgregar
-            // 
-            this.btnAgregar.Location = new System.Drawing.Point(250, 98);
-            this.btnAgregar.Name = "btnAgregar";
-            this.btnAgregar.Size = new System.Drawing.Size(59, 23);
-            this.btnAgregar.TabIndex = 5;
-            this.btnAgregar.Text = "Agregar";
-            this.btnAgregar.UseVisualStyleBackColor = true;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(18, 67);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(23, 13);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "Rol";
-            // 
-            // txtRol
-            // 
-            this.txtRol.Location = new System.Drawing.Point(115, 67);
-            this.txtRol.Name = "txtRol";
-            this.txtRol.Size = new System.Drawing.Size(100, 20);
-            this.txtRol.TabIndex = 7;
-            // 
-            // FormRol
+            // FormRolFuncionalidad
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(332, 435);
-            this.Controls.Add(this.txtRol);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.btnAgregar);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.comboBox1);
+            this.ClientSize = new System.Drawing.Size(535, 634);
             this.Controls.Add(this.btnVolver);
             this.Controls.Add(this.btnAceptar);
             this.Controls.Add(this.gridRoles);
-            this.Name = "FormRol";
+            this.Name = "FormRolFuncionalidad";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.FormRol_Load);
             ((System.ComponentModel.ISupportInitialize)(this.gridRoles)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -136,10 +83,5 @@
         private System.Windows.Forms.DataGridView gridRoles;
         private System.Windows.Forms.Button btnAceptar;
         private System.Windows.Forms.Button btnVolver;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button btnAgregar;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox txtRol;
     }
 }
---
+++ FrbaOfertas/AbmRol/FormRolFuncionalidad.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrbaOfertas.AbmRol
+{
+    public partial class FormRolFuncionalidad : Form
+    {
+        Conexion con = new Conexion();
+
+        public FormRolFuncionalidad()
+        {
+            InitializeComponent();
+        }
+
+        private void FormRol_Load(object sender, EventArgs e)
+        {
+            refreshListaFuncionalidades();
+        }
+
+        private void refreshListaFuncionalidades()
+        {
+            DataTable listaRoles = con.getRoles();
+            gridRoles.DataSource = listaRoles;
+        }
+
+        private void gridRoles_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            MenuForm menu = new MenuForm(this);
+            menu.Show();
+        }
+    }
+}
